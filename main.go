8f2d469e75c5bfc7e2fea31f61e37934970e4789
prospero78su <prospero.78.su@gmail.com>
2018-06-14 16:41:39 +0200

    04 Первая попытка добавить меню

--- main.go
+++ main.go
@@ -5,6 +5,33 @@ import (
 	"fmt"
 )
 
+func Шапка_Печать() {
+	fmt.Println("\n\033[1m   =[ Программа учёта \"РАЙАВТОДОР\" ]=\n")
+
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	var цДейств int = -1
+мЦикл:
+	for {
+		Шапка_Печать()
+		fmt.Println("\nДействие:")
+		fmt.Println("   0) Выход из программы")
+		fmt.Println("   1) Создать новую базу")
+		fmt.Println("   2) Открыть существующую базу")
+		fmt.Print("Действие > ")
+		fmt.Scanf("%d", &цДейств)
+		switch цДейств {
+		case 0:
+			fmt.Println("Конец программы")
+			break мЦикл
+		case 1:
+			fmt.Println("Создание новой базы")
+		case 2:
+			fmt.Println("Открытие новой базы")
+		default:
+			fmt.Println("Неизвестное значение")
+		}
+	}
+
 }
